98f59c547835ccfce6de3aed9370c4cc6eb9659b
JULIA\Julia <juliautenkova@gmail.com>
2017-02-12 16:38:37 +0200

    init

---
+++ src/js/components/Layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// import Footer from "./Footer";
+// import Header from "./Header";
+//
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "Welcome",
+        };
+    }
+
+    changeTitle(title) {
+        this.setState({title});
+    }
+
+    render() {
+        return (
+            <h1>{this.state.title}</h1>
+        );
+    }
+}
